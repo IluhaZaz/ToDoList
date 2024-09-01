ffc69be4499d24eaec33f1368d275561567faec3
Ilya Zazvonov <Acer@DESKTOP-5LU3HEA>
2024-09-01 14:58:51 +0300

    add: schemas.py add Iten for upd and read add: router.py add new routes for upd and deletion

--- src/core/router.py
+++ src/core/router.py
@@ -2,12 +2,13 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from sqlalchemy.orm import joinedload
+from uuid import UUID
 
-from core.schemas import ItemCreate
+from core.schemas import ItemCreate, ItemRead, ItemUpdate
 from database import get_async_session
 from core.models import Item
 from auth.models import User
-from auth.user_manager import get_user_manager, BaseUserManager
+
 
 router = APIRouter(
     prefix="/todo",
@@ -26,12 +27,50 @@ async def add_item_to_list(item: ItemCreate,
     session.add(item_db)
     await session.commit()
 
-@router.get("/get_items")
+@router.get("/get_items", response_model=list[ItemRead])
 async def get_items(user: User = Depends(current_user),
                     session: AsyncSession = Depends(get_async_session)):
     query = select(User).options(joinedload(User.to_do_items)).filter(user.id == User.id)
     user_with_items = await session.execute(query)
     user_with_items = user_with_items.unique().scalars().first()
     
-    return user_with_items.to_do_items
-    
+    res = [ItemRead.model_validate(i, from_attributes=True) for i in user_with_items.to_do_items]
+    return res
+
+@router.post("/marks_as_done")
+async def mark(item_id: UUID, 
+               user: User = Depends(current_user),
+               session: AsyncSession = Depends(get_async_session)
+               ):
+    item_db = await session.get(Item, item_id)
+
+    if item_db.user_id != user.id:
+        return
+    item_db.is_done = True
+    await session.commit()
+
+@router.patch("/update_item")
+async def update_item(item_id: UUID,
+                      new_item: ItemUpdate,
+                      session: AsyncSession = Depends(get_async_session),
+                      user: User = Depends(current_user)
+                      ):
+    item_db = await session.get(Item, item_id)
+    if item_db.user_id != user.id:
+        return
+    to_upd = new_item.model_dump()
+    for field, val in to_upd.items():
+        if val is not None:
+            setattr(item_db, field, val)
+    await session.commit()
+
+@router.delete("/delete_item")
+async def delete_item(item_id,
+                      session: AsyncSession = Depends(get_async_session),
+                      user: User = Depends(current_user)
+                      ):
+    item_db = await session.get(Item, item_id)
+    if item_db.user_id != user.id:
+        return
+    await session.delete(item_db)
+    await session.commit()
--- src/core/schemas.py
+++ src/core/schemas.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from datetime import datetime
 from pydantic import BaseModel, Field
+from uuid import UUID
 
 
 class ItemCreate(BaseModel):
@@ -8,3 +9,10 @@ class ItemCreate(BaseModel):
     comment: Optional[str]
     priority: int = Field(le=10, ge=0, default=0)
     do_till: Optional[datetime]
+
+class ItemRead(ItemCreate):
+    id: UUID
+    is_done: bool
+
+class ItemUpdate(ItemCreate):
+    name: Optional[str]
